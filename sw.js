8f6e97df1e2bcb1f75993e6691a8ddc2aa36345c
scartdesign <146715641+scartdesign@users.noreply.github.com>
2026-09-21 00:50:09 +0200

    Refresh cache for preview-66

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vidar-shell-2026-09-21-preview-65';
+const CACHE='vidar-shell-2026-09-21-preview-66';
 const SHELL=['/','/index.html','/manifest.webmanifest','/assets/icon.svg','/privacy.html','/terms.html','/data/topics.json','/data/meds.json','/data/naturals.json','/version.json','/health.json'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
